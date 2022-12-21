4093370b8077cda4419fbc3627bfa7f26c4f3580
Sushil Ghate <sushilsg30@gmail.com>
2022-12-21 12:53:01 +0530

    block-BJaaap

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,10 +3,11 @@ var http = require("http");
 var server = http.createServer(handleRequestResponse);
 
 function handleRequestResponse(req, res) {
-  res.statusCode('200');
-  res.setHeader('content-Type', 'text/plain')
-  res.writeHeader()
-    res.end("Welcome");
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'text/html');
+//   res.write('<h1>Welcome Home</h1>')
+//   res.writeHeader()
+    res.end("<h1>Welcome Home</h1>");
 }
 
 server.listen(4444, () => {
